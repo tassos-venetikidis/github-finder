c3c5ef3b71fdffb99fe9d62f937b4aeb474a2c3e
Tassos Venetikidis <tassos.venetikidis@gmail.com>
2026-05-02 16:47:24 +0300

    Implemented the basic display of the app and applied the UserSearch component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import UserSearch from "./components/UserSearch.tsx";
+
 function App() {
-  return <>My App</>;
+  return (
+    <div className="container">
+      <h1>GitHub Finder</h1>
+      <UserSearch></UserSearch>
+    </div>
+  );
 }
 
 export default App;
