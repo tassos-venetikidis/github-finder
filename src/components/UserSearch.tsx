5f30d88b3b859126152d77404bebe4446e36a84b
Tassos Venetikidis <tassos.venetikidis@gmail.com>
2026-05-04 23:27:58 +0300

    Implemented the fetching of user suggestions and applied the SuggestionDropdown component for the display of said suggestions

--- src/components/UserSearch.tsx
+++ src/components/UserSearch.tsx
@@ -1,8 +1,10 @@
 import { useState, useEffect } from "react";
 import { useQuery } from "@tanstack/react-query";
-import { fetchGithubUser } from "../api/github.ts";
+import { useDebounce } from "use-debounce";
+import { fetchGithubUser, fetchUserSuggestions } from "../api/github.ts";
 import UserCard from "./UserCard.tsx";
 import RecentSearches from "./RecentSearches.tsx";
+import SuggestionDropdown from "./SuggestionDropdown.tsx";
 
 function UserSearch() {
   const [username, setUsername] = useState("");
@@ -11,15 +13,25 @@ function UserSearch() {
     const stored = localStorage.getItem("recentUserSearches");
     return stored ? JSON.parse(stored) : [];
   });
+  const [debouncedUsername] = useDebounce(username, 300);
+  const [showSuggestions, setShowSuggestions] = useState(false);
 
   useEffect(() => {
     localStorage.setItem("recentUserSearches", JSON.stringify(recentUsers));
   }, [recentUsers]);
 
-  const { data, isLoading, isError, error } = useQuery({
+  // Query to fetch specific user
+  const { data, isLoading, isError, error, refetch } = useQuery({
     queryKey: ["users", submittedUsername],
     queryFn: () => fetchGithubUser(submittedUsername),
     enabled: !!submittedUsername,
+  });
+
+  // Query to fetch suggestions to user search
+  const { data: suggestions } = useQuery({
+    queryKey: ["github-user-suggestions", debouncedUsername],
+    queryFn: () => fetchUserSuggestions(debouncedUsername),
+    enabled: debouncedUsername.length > 1,
   });
 
   function handleSubmit(e: React.SubmitEvent) {
@@ -35,12 +47,37 @@ function UserSearch() {
   return (
     <>
       <form onSubmit={handleSubmit} className="form">
-        <input
-          type="text"
-          placeholder="Enter GitHub Username..."
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        />
+        <div className="dropdown-wrapper">
+          <input
+            type="text"
+            placeholder="Enter GitHub Username..."
+            value={username}
+            onChange={(e) => {
+              const val = e.target.value;
+              setUsername(val);
+              setShowSuggestions(val.trim().length > 1);
+            }}
+          />
+          <SuggestionDropdown
+            show={showSuggestions}
+            suggestions={suggestions}
+            onSelect={(userlogin) => {
+              setUsername("");
+              setShowSuggestions(false);
+              if (submittedUsername !== userlogin) {
+                setSubmittedUsername(userlogin);
+              } else {
+                refetch();
+              }
+              setRecentUsers((prev) => {
+                return [
+                  userlogin,
+                  ...prev.filter((item) => item !== userlogin),
+                ].slice(0, 5);
+              });
+            }}
+          />
+        </div>
         <button type="submit">Search</button>
       </form>
       {isLoading && <p className="status">Loading...</p>}
@@ -51,7 +88,7 @@ function UserSearch() {
         <RecentSearches
           users={recentUsers}
           onSelect={(user) => {
-            setUsername(user);
+            setUsername("");
             setSubmittedUsername(user);
           }}
         />
