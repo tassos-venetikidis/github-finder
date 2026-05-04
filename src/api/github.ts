e4ede17d8c119c8f3d2f9df49150fe8b756a9164
Tassos Venetikidis <tassos.venetikidis@gmail.com>
2026-05-04 23:25:01 +0300

    Added the asynchronous fetchUserSuggestions function

--- src/api/github.ts
+++ src/api/github.ts
@@ -7,4 +7,13 @@ async function fetchGithubUser(username: string) {
   return data;
 }
 
-export { fetchGithubUser };
+async function fetchUserSuggestions(inputQuery: string) {
+  const res = await fetch(
+    `${import.meta.env.VITE_GITHUB_API_URL}/search/users?q=${inputQuery}`,
+  );
+  if (!res.ok) throw new Error("User not found!");
+  const data = await res.json();
+  return data.items;
+}
+
+export { fetchGithubUser, fetchUserSuggestions };
