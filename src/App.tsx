a9983fd820b637d32f1332a73444841e27b59fbf
Tassos Venetikidis <tassos.venetikidis@gmail.com>
2026-05-07 18:20:03 +0300

    Imported and added the Toaster component of Sonner to use as the packages output in the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import UserSearch from "./components/UserSearch.tsx";
+import { Toaster } from "sonner";
 
 function App() {
   return (
     <div className="container">
       <h1>GitHub Finder</h1>
       <UserSearch></UserSearch>
+      <Toaster></Toaster>
     </div>
   );
 }
