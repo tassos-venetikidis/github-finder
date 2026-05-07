2f0d02a321a0ee2393d8fa8486868a1edbd9506b
Tassos Venetikidis <tassos.venetikidis@gmail.com>
2026-05-07 17:13:46 +0300

    Implemented the querying of the github API to check if user displayed is followed and facilitated the button display accordingly

--- src/components/UserCard.tsx
+++ src/components/UserCard.tsx
@@ -1,20 +1,41 @@
-import { FaGithubAlt } from "react-icons/fa";
+import { useQuery } from "@tanstack/react-query";
+import { checkIfFollowingUSer } from "../api/github.ts";
+import { FaGithubAlt, FaUserMinus, FaUserPlus } from "react-icons/fa";
 import type { GitHubUser } from "../types.ts";
 
 function UserCard({ user }: { user: GitHubUser }) {
+  const { data: isFollowing, refetch } = useQuery({
+    queryKey: ["follow-status", user.login],
+    queryFn: () => checkIfFollowingUSer(user.login),
+    enabled: !!user.login,
+  });
+
   return (
     <div className="user-card">
       <img src={user.avatar_url} alt={user.name} className="avatar" />
       <h2>{user.name || user.login}</h2>
       <p className="bio">{user.bio}</p>
-      <a
-        href={user.html_url}
-        className="profile-btn"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <FaGithubAlt /> View GitHub Profile
-      </a>
+      <div className="user-card-buttons">
+        <button className={`follow-btn ${isFollowing ? "following" : ""}`}>
+          {isFollowing ? (
+            <>
+              <FaUserMinus className="follow-icon" /> Following
+            </>
+          ) : (
+            <>
+              <FaUserPlus className="follow-icon" /> Follow User
+            </>
+          )}
+        </button>
+        <a
+          href={user.html_url}
+          className="profile-btn"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaGithubAlt /> View GitHub Profile
+        </a>
+      </div>
     </div>
   );
 }
